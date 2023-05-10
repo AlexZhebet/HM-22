06ba06e63776783f20abb4b5c255a4c2f75dd9f7
AlexZhebet <OleksandrZebet@gmail.com>
2023-05-10 22:36:52 +0300

    add new files1

---
+++ burger_app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MenuCreateBurger from './components/MenuCreateBurger/MenuCreateBurger';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MenuCreateBurger/>
+    </div>
+  );
+}
+
+export default App;
